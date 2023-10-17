eacbd0afa350e1a0a4477203b325b18bce8410b8
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2023-10-16 19:52:17 -0500

    Adding initial archetype

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '@popperjs/core';
+import 'bootstrap';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import './core/styles/app.scss';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<h1></h1>
+	</React.StrictMode>,
+);
