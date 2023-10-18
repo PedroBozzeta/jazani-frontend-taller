f9b7108b56996e02809e3fda0965bdea7b1d6544
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2023-10-18 17:26:03 -0500

    Adding enviroment variables

--- src/generals/MeasureUnits/infrastructure/MeasureUnitRepository.ts
+++ src/generals/MeasureUnits/infrastructure/MeasureUnitRepository.ts
@@ -1,4 +1,5 @@
 import axios, { type AxiosResponse } from 'axios';
+import { API_BASE_URL } from '@/core/constants/env';
 import { type MeasureUnitResponse } from '../domain';
 // token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YzA1YjExOS02OGFiLTQ1MDYtYjY3NS00YTIzODJhMTYzN2IiLCJpYXQiOiIxNy8xMC8yMDIzIDAxOjI2OjIwIiwibmJmIjoxNjk3NTA1OTgwLCJleHAiOjE2OTc1OTIzODB9.xc9EMP7751xrBVoj_z5aF87xG_KHJE4prC69XWeaZRg
 export const findAll = async (): Promise<MeasureUnitResponse[]> => {
@@ -9,7 +10,7 @@ export const findAll = async (): Promise<MeasureUnitResponse[]> => {
 	// return response;
 
 	const response: AxiosResponse<MeasureUnitResponse[]> = await axios.get<MeasureUnitResponse[]>(
-		'https://localhost:7281/api/measureunit',
+		`${API_BASE_URL}/api/measureunit`,
 	);
 
 	return response.data;
